4e746e8a0fb270a3187a7de266fe99a63c5edc7d
Csontos <csontostibor48@gmail.com>
2024-01-11 09:30:48 +0100

    vegleges

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Render, Param, Session } from '@nestjs/common';
+import { Controller, Get, Render, Param, Session, Res } from '@nestjs/common';
+import { Response } from 'express';
 import * as mysql from 'mysql2';
 import { AppService } from './app.service';
 import { termek } from './termekek'; 
@@ -9,22 +10,16 @@ const conn = mysql.createPool({
   port: parseInt(process.env.DB_PORT) || 3306,
   user: process.env.DB_USERNAME || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_DATABASE || 'termekek',
+  database: process.env.DB_DATABASE || 'elemek',
 }).promise();
 
 @Controller()
 export class AppController {
-  private cart: number[] = []; 
-  private products: termek[] = []; 
-
-  constructor(private readonly appService: AppService) {
-    this.loadProducts();
-  }
-
-  private async loadProducts() {
+  private async loadProducts(): Promise<termek[]> {
     try {
-      const [rows] = await conn.query<RowDataPacket[]>('SELECT * FROM termek_adatok');
-      this.products = rows.map((row: any) => Object.assign(new termek(), row));
+      const [data, fields] = await conn.query('SELECT * FROM termekek');
+      const products = Array.from(data as termek[]);
+      return products;
     } catch (error) {
       console.error('Error loading products from database:', error);
     }
@@ -32,21 +27,23 @@ export class AppController {
 
   @Get()
   @Render('index')
-  index(@Session() session: Record<string, any>) {
-    const totalValue = this.calculateTotalValue();
+  async index(@Session() session: Record<string, any>) {
+    const products = await this.loadProducts();
     const cart = session?.cart || []; 
-    return { message: 'Welcome to the homepage', products: this.products, cart, totalValue };
+    const totalValue = this.calculateTotalValue(products, cart);
+    return { message: 'Welcome to the homepage', products: products, cart, totalValue };
   }
 
-  private calculateTotalValue(): number {
-    return this.cart.reduce((total, productId) => {
-      const product = this.products.find(p => p.id === productId);
-      return total + (product ? product.ar : 0);
-    }, 0);
+  private calculateTotalValue(products: termek[], cart: number[]): number {
+    return cart.reduce((sum, cartProd) => sum + products[products.findIndex(termek => termek.id == cartProd)].ar, 0);
   }
 
   @Get('/cart/add/:id')
-  addToCart(@Param('id') id: string, @Session() session: Record<string, any>) {
+  addToCart(
+    @Param('id') id: string, 
+    @Session() session: Record<string, any>,
+    @Res() res: Response
+    ) {
     const productId = parseInt(id, 10);
 
     if (!session.cart) {
@@ -57,6 +54,6 @@ export class AppController {
      
       session.cart.push(productId);
     }
-    return { message: `Product with ID ${productId} added to the cart` };
+    res.redirect('/');
   }
 }
